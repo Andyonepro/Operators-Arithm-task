d21b6dcded68b59c3bd0a97901b94ca3a48582f6
Andyonepro <Mywork2797@gmail.com>
2023-08-28 03:10:38 +0300

    end of project

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,5 +2,42 @@ package app;
 
 public class Main {
     static String name;
+    static int quota;
+    static double price;
+    static double salesAmount;
+    static double avgSum;
+    static int days;
+    private static final String CURRENCY = "EUR";
+
+    public static void main(String[] args) {
+        name = "Oranges";
+        quota = 158;
+        price = 2.99;
+        days = 7;
+
+        salesAmount = quota * price;
+        System.out.printf("%nProduct: %s, sales %s %.2f ",name, CURRENCY,
+                salesAmount );
+
+        avgSum = salesAmount / days;
+        System.out.printf("%nProduct: %s, sales by day %s %.2f ", name, CURRENCY,
+                avgSum);
+
+        name = "Apples";
+        quota = 117;
+        price = 1.95;
+        days = 7;
+
+        salesAmount = quota * price;
+        System.out.printf("%nProduct: %s, sales %s %.2f ", name, CURRENCY,
+                salesAmount );
+
+        avgSum = salesAmount / days;
+        System.out.printf("%nProduct: %s, sales by day %s %.2f ", name,
+                CURRENCY, avgSum );
+
+    }
+
+
 
 }
